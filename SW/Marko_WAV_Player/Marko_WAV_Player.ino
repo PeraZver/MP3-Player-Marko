3493702c1668620ed9552f3a87e6929be9144ea4
PeraZver <krivic.pero@gmail.com>
2016-12-10 16:08:09 +0100

    List all files on SD card

--- SW/Marko_WAV_Player/Marko_WAV_Player.ino
+++ SW/Marko_WAV_Player/Marko_WAV_Player.ino
@@ -8,15 +8,15 @@
 
 
 #include <SD.h>
-#define SD_ChipSelectPin 17  //using digital pin 4 on arduino nano 328, can use other pins
+#define SD_ChipSelectPin 17   //using digital pin 4 on arduino nano 328, can use other pins
 #include <TMRpcm.h>           //  also need to include this library...
 #include <SPI.h>
 #define F_CPU 8000000UL // 8 MHz clock 
 
-TMRpcm tmrpcm;   // create an object for use in this sketch
+TMRpcm tmrpcm;          // create an object for use in this sketch
 uint8_t buttons = 0;
-boolean play_flag = 0;
-
+boolean play_flag = LOW;
+char * song= "";
 
 void setup(){
   /* Set system clock */
@@ -25,19 +25,38 @@ void setup(){
   CLKPR = 0b10000000;  // Change the clock prescaler
   CLKPR = 0;           // Prescaler is 1.
 
-  MCUCR |= (1<<JTD);   // Disable JTAG interface on PORTF
+  /* Disable JTAG interface on PORTF */
+  MCUCR |= (1<<JTD);   // 
   MCUCR |= (1<<JTD);   // Have to do it twice (datasheet page 328.)
-  
-  tmrpcm.speakerPin = 9; // PWM player output mono
-  pinMode(10,OUTPUT);    // and stereo
 
-  Serial.begin(9600);
-  DDRF = 0x01; // Set all pins in PORTF as input except the last one where is LED
+  delay(2000);
   
+  /* Set buttons */
+  DDRF = 0x01; // Set all pins in PORTF as input except the last one where is LED
+
+  /* Set serial communication*/
+  Serial.begin(9600);
+
+  /* Initialize SD Card*/ 
   if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    Serial.println("SD kartica nevalja. Glupane. ");
     return;   // don't do anything more if not
   }
+
+  /* Make the playlist */
+  find_music();
+ // song = entry.name();          // get the 1st song name
+  song = "FRANK002.WAV";
+  Serial.print("First song is: ");
+  Serial.println(song);
+
+  /* Set PWM output */  
+  tmrpcm.speakerPin = 9; // PWM player output mono
+  pinMode(10,OUTPUT);    // and stereo
+  
 }
+
+
 
 
 
@@ -49,8 +68,8 @@ void loop(){
  switch (buttons){
 
   case (1 << PINF7):         // "Play/Pause" button on PF7
-       if (!play_flag){     // if it's not playing, make it play
-          tmrpcm.play("PRINCE16.WAV");
+       if (!play_flag){      // if it's not playing, make it play
+          tmrpcm.play(song);
           play_flag = HIGH;
        }
        else {
@@ -58,7 +77,7 @@ void loop(){
           play_flag = LOW;
        }
 
-      toggle_LED();
+       toggle_LED();
        
        break;
        
@@ -68,42 +87,59 @@ void loop(){
        break;
        
   case (1 << PINF1):    // Volume down button on PF1
-       //tmrpcm.volume(0); 
-       tmrpcm.play("PRINCE16.WAV");
+       tmrpcm.volume(0); 
        toggle_LED();
        break;
+
+  case (1 << PINF6):    // Play next file
+      toggle_LED();
+     /* entry =  root.openNextFile();
+      song = entry.name();
+      Serial.print("Now playing: ");
+      Serial.println(song);
+      tmrpcm.play(song);*/
+      break;
       
   default:
       break;
   }
   
- /*   switch(ser){
-      case 'p':
-          tmrpcm.play("PRINCE8.WAV");
-          break;
-      case 'i':
-          tmrpcm.play("PRINCE11.WAV");
-          break;
-      case 'r':
-          tmrpcm.play("PRINCE16.wav");
-          break;
-      case 'c':
-          tmrpcm.play("PRINCE32.wav");
-          break;
-       case 'e':
-          tmrpcm.play("PRINCE44.wav");
-          break;
-      default:
-        delay(10);
-  }*/
-
 delay(500);
 }
 
 void toggle_LED(void){
+/* Simple LED toggler for test purposes */ 
        PORTF |= (1 << PINF0);
        delay(500);
        PORTF &= ~(1 << PINF0);
   
+}
+
+void find_music(){
+/* This function searches the root folder of SD card 
+ *  and gets the filenames of WAV files.
+ */
+
+  File root;        // Grab the file system object
+  /* Get the filenames */
+  root = SD.open("/");  
+  while(true) {
+     
+     File entry =  root.openNextFile();
+     if (! entry) {
+       // no more files
+       break;
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       //printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
 }
 
