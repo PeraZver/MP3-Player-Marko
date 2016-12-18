aa4c0fa2fa74e3926aedc99dc50792928a31ef53
PeraZver <krivic.pero@gmail.com>
2016-12-18 01:46:35 +0100

    read long filename using SdFatLib

--- SW/Marko_WAV_Player/Marko_WAV_Player.ino
+++ SW/Marko_WAV_Player/Marko_WAV_Player.ino
@@ -1,14 +1,24 @@
-/*****************************
+#include <BlockDriver.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <SdFatConfig.h>
+#include <SysCall.h>
+
+
+#include <SdFat.h>
+
+
+/****************************************
  * 
  * Marko WAV Player with an OLED display
  * 
  * Pero, Dec. 2016 
  * 
- */
+ ****************************************/
 
 #include <SPI.h>
 #include <Wire.h>
-#include <SD.h>
+//#include <SD.h>
 #include <TMRpcm.h>           
 #include <string.h>
 #include <Adafruit_GFX.h>
@@ -20,119 +30,71 @@
 #define OLED_DC    6
 #define OLED_CS    4
 #define OLED_RESET  12
-#define SD_ChipSelectPin 17  //using digital pin 4 on arduino nano 328, can use other pins 
+#define SD_ChipSelectPin 17  
 
-// Classes for OLED display and SD card
+// Classes for TMRpcm player and OLED
+SdFat sd;
+TMRpcm tmrpcm;     
 //Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
-TMRpcm tmrpcm;   // create an object for use in this sketch
 
 uint8_t buttons = 0;
 boolean play_flag = LOW, start_playing = LOW;
-char *song[10]={};        // Playlist stored as an array of strings i.e. pointers to the char
-uint8_t songCtr = 0;      // Number of songs 
-uint8_t currentSong = 0;  // Current song in the playlist, start from 0.
+char *song[10]={};          // Playlist stored as an array of strings i.e. pointers to the char
+char song_name[50]={'0'};   // Song name for OLED
+uint8_t songCtr = 0;        // Number of songs 
+uint8_t currentSong = 0;    // Current song in the playlist, start from 0.
 
-// defs for OLED
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
 
 void setup(){
-  /* Set system clock */
-  CLKSEL0 = 0b00010101;   // Choose Crystal oscillator with BOD
-  CLKSEL1 = 0b00001111;   // 8MHz
-  CLKPR = 0b10000000;  // Change the clock prescaler
-  CLKPR = 0;           // Prescaler is 1.
 
-  /* Disable JTAG interface on PORTF */
-  MCUCR |= (1<<JTD);   // 
-  MCUCR |= (1<<JTD);   // Have to do it twice (datasheet page 328.)
-
-  /* Set buttons */
-  DDRF = 0x01; // Set all pins in PORTF as input except the last one where is LED
-
-  /* Set serial communication*/
-  Serial.begin(9600);
-
+  AVRSetup();
+  
   /* Initialize SD Card*/ 
-  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
-     Serial.println("SD kartica nevalja. Glupane. ");
-    return;   // don't do anything more if not
+  if (!sd.begin(SD_ChipSelectPin)) {    // see if the card is present and can be initialized:
+    return;                             // don't do anything more if not
   }
-
+  
   // Generate the OLED supply from the 3.3v line internally
-  // display.begin(SSD1306_SWITCHCAPVCC);  // Show image buffer on the display hardware.
-  //display.display();
-  /*delay(2000);
-  display.clearDisplay();    */
-  
-  delay(4000);
-  
-  /* Make the playlist */
-  uint8_t i = 0;  // helpful counter
-  find_music();
-  Serial.println("Playlist: ");
-  for (i=0; i<5; i++)
-     Serial.println(*(song+i));
+  //display.begin(SSD1306_SWITCHCAPVCC);  // Show image buffer on the display hardware.
 
-  Serial.print("First song is: ");
-  Serial.println(song[currentSong]);
+  Serial.begin(9600);
+  delay(5000);  
+  /* Make the playlist */
+  uint8_t i=0;
+  find_music();            // creates list of WAV files for player
+  Serial.println("Playlist: ");
+  for (i=0; i<5; i++){
+     Serial.println(*(song+i));
+    // ReadLine(song_name, i);
+    // Serial.println(song_name);
+  }
 
   /* Set PWM output */  
   tmrpcm.speakerPin = 9; // PWM player output mono
   pinMode(10,OUTPUT);    // and stereo
 
-  Serial.print("Number of songs: ");
-  Serial.println(NumberOfSongs());
-  char pjesma[50]={'0'};
-  
-  for (i = 1; i <= 5; i++){
-      ReadLine(pjesma, i);      // in array pjesma store the i-th line in the textfile.
-      Serial.print("Pjesma br.");
-      Serial.print(i);
-      Serial.print(" je: ");
-      Serial.print(pjesma);
-  }    
-  //free(pjesma);
-  
-  Serial.print(pjesma);
-  if (SD.exists(pjesma)) {
-    Serial.println(" exists.");
-  }
-  else {
-    Serial.println("doesn't exist.");
-  }
 }
-
-
-
-
 
 void loop(){  
 
- buttons = PINF;
- /* Check the buttons first */
+// buttons = PINF;
+// /* Check the buttons first */
 // switch (buttons){
 //
 //  case (1 << PINF7):         // "Play/Pause" button on PF7
 //       if (!play_flag & !start_playing){      // if it's not playing, make it play
 //          tmrpcm.play(song[currentSong]);
-//          /*Serial.print("Started playing: ");
-//          Serial.println(song[currentSong]);  */
-//          testscrolltext(song[currentSong]); // Scroll some text.
+//          //ReadLine(song_name, currentSong);
+//          //Serial.println(song_name);
+//          //PrintToOLED(song_name); 
 //          play_flag = start_playing = HIGH;
 //       }
 //       else if (!play_flag & start_playing){
 //          tmrpcm.pause();
-//         // Serial.println("unpause");
 //          play_flag = HIGH;
 //       }
 //       else {
 //          tmrpcm.pause();
-//        //  Serial.println("pause");
 //          play_flag = LOW;
 //       }
 //       toggle_LED();       
@@ -153,9 +115,9 @@ void loop(){
 //      if (currentSong < songCtr){
 //          currentSong++;
 //          tmrpcm.play(song[currentSong]);
-//          testscrolltext(song[currentSong]); // Scroll some text.
-//          /*Serial.print("Now playing: ");
-//          Serial.println(song[currentSong]);*/
+//          //ReadLine(song_name, currentSong);
+//          //Serial.println(song_name);
+//          //PrintToOLED(song_name); 
 //      }
 //      break;
 //
@@ -163,10 +125,10 @@ void loop(){
 //      toggle_LED();
 //      if (currentSong > 0){
 //          currentSong--;
-//          tmrpcm.play(song[currentSong]);     
-//          testscrolltext(song[currentSong]); // Scroll some text.     
-//          /*Serial.print("Now playing: ");
-//          Serial.println(song[currentSong]);*/
+//          tmrpcm.play(song[currentSong]);
+//          //ReadLine(song_name, currentSong);          
+//          //Serial.println(song_name);
+//          //PrintToOLED(song_name);    
 //      }
 //      break;     
 //
@@ -175,16 +137,16 @@ void loop(){
 //      break;
 //  }
 //  
-// delay(500);
-
-  /* Keep on with the playlist */
+// delay(100);
+//
+//  /* Keep on with the playlist */
 //  if (start_playing & !tmrpcm.isPlaying()){  //If the playlist is started, and one song has finished, continue to the next song
 //   if (currentSong < songCtr){               // if the list has not come to an end
 //          currentSong++;
 //          tmrpcm.play(song[currentSong]);
-////         testscrolltext(song[currentSong]); // Scroll some text.
-//          /*Serial.print("Now playing: ");
-//          Serial.println(song[currentSong]);*/
+//          //ReadLine(song_name, currentSong);
+//          //Serial.println(song_name);
+//          //PrintToOLED(song_name); 
 //      }
 //    else {
 //      currentSong = 0;                 // if the list has come to an end, reset the counter
@@ -208,54 +170,35 @@ void find_music(){
 /* This function searches the root folder of SD card 
  *  and gets the filenames of WAV files.
  */
-  File root;        // Grab the file system object
+  SdFile root;        // Grab the file system object
+  SdFile entry;
   /* Get the filenames */
-  root = SD.open("/");  
-  while(true) {
+  root.open("/", O_READ  );  
+  while(entry.openNext(&root, O_READ)) {     
      
-     File entry =  root.openNextFile();
-     if (! entry) {
-       // no more files
-       break;
-     }
-
-     if (entry.isDirectory()== 0) {         // If the entry is not a directory
-         song[songCtr] = strdup(entry.name());  // add it to the the string array
-         songCtr++;                            
-     }
-     entry.close();  
-   }
-   root.close(); 
+     // Skip directories and hidden files.
+    if (!entry.isSubDir() && !entry.isHidden()) {    
+        //entry.printName(&Serial);
+        entry.getName(song_name, 40);
+        Serial.println(song_name);
+    }
+    entry.close();
+   //root.close(); 
 }
-
-//void testscrolltext(char* text) {
+}
+//void PrintToOLED(char* text) {
+//  
+//
 //  display.clearDisplay(); 
 //  display.setTextSize(1);
 //  display.setTextColor(WHITE);
-//  display.setCursor(10,10);
-//  display.clearDisplay();
+//  display.setCursor(0,0);
+//  //display.clearDisplay();
 //  display.println(text);
 //  display.display();
-//  display.startscrollright(0x00, 0x0F);
+//  //display.startscrollright(0x00, 0x0F);
 //}
 
-uint8_t NumberOfSongs(){
-  /* Function that finds the number of lines (songs) in the text file*/
-    File playlist;
-    playlist = SD.open("playlist.txt");
-    
-    uint8_t lineCounter = 0;
-    char Ch;
-   
-    while (playlist.available()) {
-
-        Ch = playlist.read();   // read char by char in file        
-        if ((Ch == '\n'))        // when we reach the end of the line
-            lineCounter++;               // count the line        
-    }
-    playlist.close();
-    return lineCounter;    // Count the last row
-}
 
 char ReadLine(char *line, uint8_t lineNumber){
 
@@ -263,7 +206,7 @@ char ReadLine(char *line, uint8_t lineNumber){
  * and stores it in the char array. */
 
     File playlist;
-    playlist = SD.open("playlist.txt");
+    playlist = sd.open("playlist.txt");
 
     //char line[20];
     uint8_t lineCounter = 0, i = 0;
@@ -280,5 +223,20 @@ char ReadLine(char *line, uint8_t lineNumber){
         }
     }
     playlist.close();   
+}
+
+void AVRSetup(){
+    /* Set system clock */
+  CLKSEL0 = 0b00010101;   // Choose Crystal oscillator with BOD
+  CLKSEL1 = 0b00001111;   // 8MHz
+  CLKPR = 0b10000000;     // Change the clock prescaler
+  CLKPR = 0;              // Prescaler is 1.
+
+  /* Disable JTAG interface on PORTF */
+  MCUCR |= (1<<JTD);   // 
+  MCUCR |= (1<<JTD);   // Have to do it twice (datasheet page 328.)
+
+  /* Set buttons */
+  DDRF = 0x01; // Set all pins in PORTF as input except the last one where is LED
 }
 
--- Troubleshooting.md
+++ Troubleshooting.md
@@ -17,17 +17,19 @@ What happened:
 
   ---------------------------------------------------------
 
-  What next:
+What next:
    - SD card examined - works properly.
    - Maybe the fuse bits are misconfigured? 
    - Brown-out detection is set low for some reason?
 
- Check 3V3 again: it's low 3.0V. Set external power supply to 3V3 rails: DEVICE WORKS NORMALLY AGAIN!
+ Check 3V3 again: it's low 3.0V. 
+ Try adding changing capacitor and adding more capacitance - doesn't help.
+ Set external power supply to 3V3 rails: DEVICE WORKS NORMALLY AGAIN!
 
  Conclusion:
    - bad power supply!
 
- Cause:
-  Input voltage of 3V3 LDO regulator is about 3V. Voltage drop on D2 is 2V. This is way too much, maybe the diode is burnt?
+Cause:
+  - Input voltage of 3V3 LDO regulator is about 3V. Voltage drop on D2 is 2V. This is way too much, maybe the diode is burnt?
 
- Bridge the diode: Device works normally again even with USB power supply.
+ Bridge the diode: Device works normally again even with USB power supply. Probably the diode burned during the accidental shortening of Vin on LDO.
